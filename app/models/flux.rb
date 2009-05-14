7d7266790657cd3f0165d525a7cf234d3ed3011d
Sven Bohm <bohms@msu.edu>
2009-05-14 15:32:38 -0400

    equation error

--- app/models/flux.rb
+++ app/models/flux.rb
@@ -48,7 +48,7 @@ class Flux < ActiveRecord::Base #CachedModel
   def headspace
     if 'Z' == incubation.lid 
       // compute gas bucket volume
-      return 1/4 * Math::PI * (26 + 0.094697 * )^2 * (incubation.avg_height_cm - 1) # one cm from the top of the bucket to the mark
+      return 1/4 * Math::PI * (26 + 0.094697 * (incubation.avg_height_cm - 1))^2 * (incubation.avg_height_cm - 1) # one cm from the top of the bucket to the mark
     else
       begin
         (incubation.avg_height_cm * 745)/1000 + self.incubation.lid.volume

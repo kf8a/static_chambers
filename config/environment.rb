47ad9a2a6f95ab3eeeb1182d1514691d7c464dc5
Sven Bohm <bohms@msu.edu>
2009-07-15 21:11:38 -0400

    moved to gems on factory girl and shoulda

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,9 @@ Rails::Initializer.run do |config|
   config.gem 'ruby-openid', :lib => 'openid' ,  :source => 'http://gems.github.com'  
   config.gem 'authlogic',    :source => 'http://gems.github.com'   #, :version => '1.4.3'
   config.gem "authlogic-oid", :lib => "authlogic_openid", :source => 'http://gems.github.com'  
+  
+  config.gem "thoughtbot-shoulda", :lib => 'shoulda', :source => 'http://gems.github.com'
+  config.gem "thoughtbot-factory_girl", :lib    => "factory_girl", :source => "http://gems.github.com"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.

741c85dafd5865928e4b6eeadc260ef259fc5eba
Sven Bohm <bohms@msu.edu>
2009-10-23 11:11:21 -0400

    use the right operator

--- app/models/run.rb
+++ app/models/run.rb
@@ -33,7 +33,7 @@ class Run < ActiveRecord::Base
     reader.shift
     self.sampled_on = reader.shift[0]
     reader.shift
-    reader.shift if name ~= /GLBRC/
+    reader.shift if name =~ /GLBRC/
     self.save
     reader.each do | row |
       next if row[0].nil?      

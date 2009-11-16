fb9563e5861c8a3f1af13a43a18371f8272bb9e0
Sven Bohm <bohms@msu.edu>
2009-11-16 13:48:40 -0500

    scale x values

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -20,6 +20,8 @@ module RunsHelper
       maxy = flux.maxy * m
       maxx = flux.maxx
       
+      x_scale = 250 / maxx
+      
       #maxy += (maxy * 0.2)
       #maxx += (maxx * 0.2)
       #maxy = 100 if maxy < 100
@@ -37,12 +39,12 @@ module RunsHelper
       dot_size = ((maxy)*0.05)
       dot_size = 2 if dot_size < 2
 
-      output = Fluxplot.new(200,200,maxx, maxy)
+      output = Fluxplot.new(200,200,maxx * x_scale, maxy)
       flux.samples.each do |sample|
         next if sample.ppm.nil?
-        output.add_point(sample.seconds, sample.ppm*m, flux.id, sample.excluded)
+        output.add_point(sample.seconds* x_scale, sample.ppm*m, flux.id, sample.excluded)
       end
-      output.add_line(0, intercept, maxx, y2)
+      output.add_line(0, intercept, maxx * x_scale, y2)
       
       output.add_line(0,maxy,-10,maxy)
       output.to_s

1e32b58d16e98a33e091ad925175f2a3b8505cdc
Sven Bohm <bohms@msu.edu>
2010-02-25 09:42:15 -0500

    shoulda and factory girl only in test

--- config/environment.rb
+++ config/environment.rb
@@ -11,8 +11,6 @@ Rails::Initializer.run do |config|
   config.gem 'authlogic',    :source => 'http://gems.github.com'   #, :version => '1.4.3'
   config.gem "authlogic-oid", :lib => "authlogic_openid", :source => 'http://gems.github.com'  
   
-  config.gem "thoughtbot-shoulda", :lib => 'shoulda', :source => 'http://gems.github.com'
-  config.gem "thoughtbot-factory_girl", :lib    => "factory_girl", :source => "http://gems.github.com"
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
